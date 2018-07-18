714baf4b495dfd3efa7810b93ecf0f655a1d32b1
buns-li <buns.zpli@gmail.com>
2018-07-19 06:42:55 +0800

    新增socket的功能

--- src/views/doc/import_of_pc.vue
+++ src/views/doc/import_of_pc.vue
@@ -4,10 +4,11 @@
 
 <template>
 	<section rs-view="doc-pc-import">
-		<p class="status-text animated zoomInDown">{{status}}</p>
+		<p class="status-text animated zoomInDown">{{statusText}}</p>
 		<img class="animated fadeInDown"
 		     :src="getLocalMduImg('doc','import_bg')" />
-		<div class="step step-1">
+		<div class="step step-1"
+		     v-show="status ===0">
 			<p class="title">
 				<span>步骤一</span>
 			</p>
@@ -16,7 +17,8 @@
 				<p class="url">{{url}}</p>
 			</div>
 		</div>
-		<div class="step step-2">
+		<div class="step step-2"
+		     v-show="status ===0">
 			<p class="title">
 				<span>步骤二</span>
 			</p>
@@ -41,7 +43,21 @@
 				</rx-col>
 			</rx-row>
 		</div>
-		<div class="fixed-footer">
+		<div class="step step-3"
+		     v-show="status!==0">
+			<p class="title">
+				<span>步骤三</span>
+			</p>
+			<div class="second-title">
+				<p>请从电脑选择要导入的文档</p>
+			</div>
+			<div class="img">
+				<img class="animated fadeInLeft"
+				     :src="getLocalMduImg('doc','pc-step3')" />
+			</div>
+		</div>
+		<div class="fixed-footer"
+		     v-show="status===0">
 			<div class="tips">
 				<p>在电脑上导入的文档将保存至“我的导入”中</p>
 				<p>由于网络状况差异，“我的导入”中文件列表更新可能有少许延迟</p>
@@ -54,16 +70,30 @@
 		name: "ImportOfPC",
 		data() {
 			return {
-				status: "等待传输",
+				status: 0,
 				isRefreshing: false,
 				code: ""
 			};
 		},
 		computed: {
+			statusText() {
+				switch (this.status) {
+					case 1:
+						return "已连接成功";
+					case 2:
+						return "已上传成功";
+					case 0:
+					default:
+						return "等待传输";
+				}
+			},
+			socketUrl() {
+				return this.$isProd ? "" : "";
+			},
 			url() {
-				return this.$isDev || this.$isTest
-					? "http://whrdd.f3322.net:8889/page/docImport/docImportLogin.html"
-					: "http://manage.jxdangjian.com/doc-import";
+				return this.$Prod
+					? "http://manage.jxdangjian.com/page/docImport/docImportLogin.html"
+					: "http://whrdd.f3322.net:8889/page/docImport/docImportLogin.html";
 			}
 		},
 		methods: {
@@ -87,6 +117,30 @@
 		},
 		mounted() {
 			this.__fetchCode();
+
+			this.$rxUtils.loadJS(
+				"https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js",
+				{},
+				() => {
+					const socket = io(this.socketUrl);
+
+					socket.on("connect", () => {
+						socket.on(`${this.authInfo.userId}`, msg => {
+							this.status = 1;
+						});
+
+						socket.on(`down${this.authInfo.userId}`, msg => {
+							// my msg
+							if (typeof msg === "string") {
+								msg = JSON.parse(msg);
+							}
+							if (msg.uploadState === 1) {
+								this.status = 2;
+							}
+						});
+					});
+				}
+			);
 		}
 	};
 </script>
