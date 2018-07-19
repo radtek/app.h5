4befe1e40124dfbfbde76c0120a6f89b0ee6f1cc
buns-li <buns.zpli@gmail.com>
2018-07-19 23:28:25 +0800

    移除rx-col组件的相关s-colc-col组件的相关span设置

--- src/views/score/mall.vue
+++ src/views/score/mall.vue
@@ -14,17 +14,16 @@
 					        align="center">
 						<rx-col class="list_item"
 						        v-for="(item,idx) in project"
-						        :key="idx"
-						        :span="6">
+						        :key="idx">
 							<div class="wrap-img">
 								<img :src="getLocalMduImg('score',item.src)">
 							</div>
 							<p class="title">{{item.title}}</p>
 							<rx-row class="footer">
-								<rx-col :span="6">
+								<rx-col>
 									<span class="strong">{{item.cost}}</span>红心
 								</rx-col>
-								<rx-col :span="6">
+								<rx-col>
 									<span v-if="item.exchangedCount">已有{{item.exchangedCount}}人兑换</span>
 									<span v-else>暂无兑换</span>
 								</rx-col>
@@ -35,7 +34,12 @@
 			</rx-tab-pane>
 			<rx-tab-pane label="我的兑换"
 			             :index="1">
-				<div class="temp-div">兑换功能尚未开放,请耐心等待!</div>
+				<div class="temp-div">兑换功能尚未开放,请耐心等待!
+
+					<canvas ref="cva"
+					        style="width:300px;height:300px;"></canvas>
+
+				</div>
 			</rx-tab-pane>
 		</rx-tab>
 	</section>
