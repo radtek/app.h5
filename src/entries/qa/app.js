2a1460dcd6dd37291c9da9ac3f93731094b04a5c
buns-li <buns.zpli@gmail.com>
2018-07-24 06:07:19 +0800

    注册jxrsapi的交互事件action

--- src/entries/qa/app.js
+++ src/entries/qa/app.js
@@ -1,6 +1,15 @@
 import Vue from "vue";
 import { createVue } from "~e/base";
 import { createRoutes } from "~r/qa/";
+import { createJSApi } from "~j/index";
 import http from "~a/qa/";
+
+createJSApi(
+	"refreshAppStatusOfQuesCollect",
+	"refreshAppStatusOfAnswer",
+	"refreshH5IMInfo",
+	"share",
+	"addIMFriend"
+);
 
 createVue(Vue, { router: createRoutes(Vue), http });
