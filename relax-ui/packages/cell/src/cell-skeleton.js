b268840353e6a435e16543bc6485b75c83d3ea95
buns-li <buns.zpli@gmail.com>
2018-07-24 23:43:17 +0800

    调整rx-cell的骨架样式

--- relax-ui/packages/cell/src/cell-skeleton.js
+++ relax-ui/packages/cell/src/cell-skeleton.js
@@ -11,13 +11,17 @@ export default {
 				h("div", { class: "rx-cell-header" }, [h("p", { class: "block" })]),
 				h("div", { class: "rx-cell-body" }, [
 					h("div", { class: "rx-cell-content" }, [h("p", { class: "block" })]),
-					h("div", { class: "rx-cell-imgs" }, [
-						h("p", { class: "block" }),
-						h("p", { class: "block" }),
-						h("p", { class: "block" })
+					h("div", { class: "rx-cell-imgs  rx-row grid" }, [
+						h("p", { class: "rx-col rx-col-8" }, [h("p", { class: "block" })]),
+						h("p", { class: "rx-col rx-col-8" }, [h("p", { class: "block" })]),
+						h("p", { class: "rx-col rx-col-8" }, [h("p", { class: "block" })])
 					])
 				]),
-				h("div", { class: "rx-cell-footer" }, [h("p", { class: "block" })])
+				h("div", { class: "rx-cell-footer rx-row grid" }, [
+					h("p", { class: "rx-col rx-col-3" }, [h("p", { class: "block" })]),
+					h("p", { class: "rx-col rx-col-3" }, [h("p", { class: "block" })]),
+					h("p", { class: "rx-col rx-col-3" }, [h("p", { class: "block" })])
+				])
 			]
 		);
 	}
