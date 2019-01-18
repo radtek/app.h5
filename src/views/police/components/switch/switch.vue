d598b67d70e3f8769af1db1be69ae18c79cdaabb
Administrator <2269629396@qq.com>
2019-01-18 14:15:34 +0800

    添加switch组件

---
+++ src/views/police/components/switch/switch.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="contain">
+		<input id="switch" class="mui-switch mui-switch-animbg" type="checkbox" @click="test">
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "switchBut",
+		methods:{
+			test(){
+				console.log(!document.getElementById('switch').checked ? "未选中" : "选中");
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.contain{
+		width: 124px;
+		.mui-switch {
+			width: 104px;
+			height: 62px;
+			position: relative;
+			border: 1px solid #dfdfdf;
+			background-color: #fdfdfd;
+			box-shadow: #dfdfdf 0 0 0 0 inset;
+			border-radius: 40px;
+			border-top-left-radius: 40px;
+			border-top-right-radius: 40px;
+			border-bottom-left-radius: 40px;
+			border-bottom-right-radius: 40px;
+			background-clip: content-box;
+			display: inline-block;
+			-webkit-appearance: none;
+			user-select: none;
+			outline: none; }
+		.mui-switch:before {
+			content: '';
+			width: 58px;
+			height: 58px;
+			position: absolute;
+			top: 0px;
+			left: 0;
+			border-radius: 40px;
+			border-top-left-radius: 40px;
+			border-top-right-radius: 40px;
+			border-bottom-left-radius: 40px;
+			border-bottom-right-radius: 40px;
+			background-color: #fff;
+			box-shadow: 0 2px 6px rgba(0, 0, 0, 0.4); }
+		.mui-switch:checked {
+			border-color:#FF6936;
+			box-shadow: #FF6936 0 0 0 16px inset;
+			background-color: #FF6936; }
+		.mui-switch:checked:before {
+			left: 42px; }
+		.mui-switch.mui-switch-animbg {
+			transition: background-color ease 0.4s; }
+		.mui-switch.mui-switch-animbg:before {
+			transition: left 0.3s; }
+		.mui-switch.mui-switch-animbg:checked {
+			box-shadow: #dfdfdf 0 0 0 0 inset;
+			background-color:#FF6936;
+			transition: border-color 0.4s, background-color ease 0.4s; }
+		.mui-switch.mui-switch-animbg:checked:before {
+			transition: left 0.3s; }
+		.mui-switch.mui-switch-anim {
+			transition: border cubic-bezier(0, 0, 0, 1) 0.4s, box-shadow cubic-bezier(0, 0, 0, 1) 0.4s; }
+		.mui-switch.mui-switch-anim:before {
+			transition: left 0.3s; }
+		.mui-switch.mui-switch-anim:checked {
+			box-shadow:#FF6936 0 0 0 16px inset;
+			background-color: #FF6936;
+			transition: border ease 0.4s, box-shadow ease 0.4s, background-color ease 1.2s; }
+		.mui-switch.mui-switch-anim:checked:before {
+			transition: left 0.3s;
+		}
+	}
+	
+	
+</style>
--- src/views/police/create-activities/activity-time/activity-time.vue
+++ src/views/police/create-activities/activity-time/activity-time.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-		<Header :title="mainTitle" :right="rightTitle" :left="left" @change="change"></Header>
+		<Header :title="mainTitle" :right="rightTitle"  @change="change"></Header>
 		<div class="time-contain">
 			<img class="delImg" v-if="rightTitle==='完成'" src="@/assets/imgs/police/866.png">
 			<div class="time-font" :class="{'active':rightTitle==='完成'}">周一20：00</div>
-			<img class="butImg" src="@/assets/imgs/police/change.png"></img>
+			<SwitchBut></SwitchBut>
 		</div>
 		<img class="addImg" v-if="rightTitle==='编辑'" src="@/assets/imgs/police/add.png"></img>
+	
 	</div>
 </template>
 
@@ -19,51 +20,47 @@
 				import(/* webpackChunkName:"wc-calendar" */ "@/views/police/components/header/header.vue").then(
 					utils.fixAsyncCmpLifeCycle
 				),
+			SwitchBut: () =>
+				import(/* webpackChunkName:"wc-calendar" */ "@/views/police/components/switch/switch.vue").then(
+					utils.fixAsyncCmpLifeCycle
+				),
 		},
 		data(){
 			return{
 				mainTitle:'活动时间',
 				rightTitle:'编辑',
-				left:''
+				on:false
 			}
 		},
 		methods:{
 			change(e){
 				this.rightTitle=e
-				if(e==='完成'){
-					this.left='删除'
-				}else{
-					this.left=''
-				}
-			}
+			},
+			
 		}
 	};
 </script>
 
 <style lang="scss" scoped>
-  		.time-contain{
+  		.time-contain {
 			height: 130px;
 			margin: 0 29px;
-			border-bottom: 1px solid rgba(230,230,230,1);
+			border-bottom: 1px solid rgba(230, 230, 230, 1);
 			display: flex;
 			align-items: center;
 			justify-content: space-between;
-			.time-font{
-				font-size:32px;
-				font-family:PingFang-SC-Bold;
-				font-weight:bold;
-				color:rgba(153,153,153,1);
+			.time-font {
+				font-size: 32px;
+				font-family: PingFang-SC-Bold;
+				font-weight: bold;
+				color: rgba(153, 153, 153, 1);
 			}
-			.delImg{
+			.delImg {
 				width: 58px;
 				height: 58px;
 			}
-			.active{
+			.active {
 				margin-right: 200px;
-			}
-			.butImg{
-				width: 102px;
-				height: 62px;
 			}
 		}
 	.addImg{
@@ -72,4 +69,6 @@
 		width: 179px;
 		height: 179px;
 	}
+
+		
 </style>
