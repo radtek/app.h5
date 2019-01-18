e6289b2b14d60d85477765ee7a99eb2ab4391bfc
金振南 <516636965@qq.com>
2019-01-18 15:08:05 +0800

    主页样式、交互初步完成

--- src/components/police/sign-up.vue
+++ src/components/police/sign-up.vue
@@ -12,9 +12,7 @@
                 </div>
             </div>
             <div class="main">
-                <span class="left"><div class="head"></div>
-                                   <div class="head"></div>
-                                   <div class="head"></div></span>
+                <span class="left"><div class="head"></div><div class="head"></div><div class="head"></div></span>
                 <div class="right"><img :src="getLocalMduImg('police','button')"></div>
             </div>
             <div class="footer">5人请假，还可以抢名额</div>
@@ -22,24 +20,35 @@
         <div class="sign-content">
             <div class="header">
                 <span class="left">2019-01-14  &nbsp&nbsp周一 &nbsp&nbsp20:00 &nbsp（16/21人已报名）</span>
-                <span class="right">查看全部</span>
+                <div class="right">
+                    <span>查看全部</span>
+                    <div class="img"><img :src="getLocalMduImg('police','quanbu2')"></div>
+                </div>
             </div>
             <div class="main">
-                <span class="left">头像头像头像头像头像头像头像头像</span>
-                <span class="right"></span>
+                <span class="left"><div class="head"></div><div class="head"></div><div class="head"></div></span>
+                <div class="right"><img :src="getLocalMduImg('police','button')"></div>
             </div>
             <div class="footer">5人请假，还可以抢名额</div>
         </div>
         <div class="sign-content">
             <div class="header">
                 <span class="left">2019-01-14  &nbsp&nbsp周一 &nbsp&nbsp20:00 &nbsp（16/21人已报名）</span>
-                <span class="right">查看全部</span>
+                <div class="right">
+                    <span>查看全部</span>
+                    <div class="img"><img :src="getLocalMduImg('police','quanbu2')"></div>
+                </div>
             </div>
             <div class="main">
-                <span class="left">头像头像头像头像头像头像头像头像</span>
-                <span class="right"></span>
+                <span class="left"><div class="head"></div><div class="head"></div><div class="head"></div></span>
+                <div class="right"><img :src="getLocalMduImg('police','button')"></div>
             </div>
             <div class="footer">5人请假，还可以抢名额</div>
+        </div>
+        <div class="more">
+            <div class="more-text">没有更多活动了，快来
+                <div class="more-button"><span>立即创建</span></div></div>
+            
         </div>
     </div>
 </template>
--- src/views/police/index.vue
+++ src/views/police/index.vue
@@ -6,33 +6,48 @@
   <div class="body">
       <main class="content">
       <div class="backpic"><img :src="getLocalMduImg('police','yujia')"></div>
-      <div class="nav"></div>
+      <div class="nav">
+        <span v-if="!edit" @click="inEdit()">编辑</span>
+        <span v-else @click="inFulfil()">完成</span>
+        </div>
       <div class="column">
-        <div class="column-input">
-          <div class="column-text">
-            <p class="column-name">活动名称：瑜伽活动</p>
-            <p class="column-action">活动地点：江西省南昌市新建区赣江南大道1366号省公安厅201室</p>
+        <div :class="{'column-input':true,'editActive':edit}">
+          <div class="column-text" v-for="i in rows">
+            <div class="column-name">
+              <span>活动名称：</span> 
+              <div v-if="!edit">{{i.name}}</div>
+              <textarea v-model="i.name" v-else rows="1"></textarea></div>
+            <div class="column-action">
+              <span>活动地点：</span> 
+              <div v-if="!edit">{{i.action}}</div>
+              <textarea v-model="i.action" v-else rows="2"></textarea></div>
           </div>
         </div>
-        <div class="column-icon"><div>抢名额</div>
+        <div class="column-icon"
+             @click="dialogJoin"><div>抢名额</div>
           <img :src="getLocalMduImg('police','minge')"> 
-          
         </div>
         
         <div class="column-footer">
           <div class="jion">已有<span class="red">112</span>人参与活动 &nbsp
           <span class="red">查看全部</span>
           <p class="img"><img :src="getLocalMduImg('police','quanbu')"></p></div>
-          <div class="tips">温馨提示：排课按顺序循环进行</div>
+          <div class="tips">温馨提示：您最近的课程是 <span class='red'>2019-01-14 周一20：00</span></div>
         </div>
       </div>
       <sign-up class="sign-up"></sign-up>
     </main>
  </div>
-  <footer>
+  <footer :class="[isWebp()?'webp':'']">
     <div class="left">分享</div>
-    <div class="right">请假</div>
+    <div class="right" @click="dialogLeave()">请假</div>
   </footer>
+  <dialog-leave v-if="leave"
+                @doCancel="isLeaveCancel"
+                @doSubmit="isLeaveSubmit"></dialog-leave>
+  <dialog-join  v-if="join"
+                @doCancel="isJoinCancel"
+                @doSubmit="isJoinSubmit"></dialog-join>
 </div>
 </template>
 
@@ -42,9 +57,55 @@
     components: {
       signUp: () =>
             import(/* webpackChunkName: "signUp" */ "~c/police/sign-up.vue"),
-            
-            
-    }
+      dialogLeave: () =>
+            import(/* webpackChunkName: "signUp" */ "~v/police/__wc__/leave-index.vue"),
+      dialogJoin: () =>
+            import(/* webpackChunkName: "signUp" */ "~v/police/__wc__/join-index.vue"),
+
+    },
+    data(){
+      return {
+      edit:false,
+      rows:[],
+      leave:false,
+      join:false,
+      }
+    },
+    methods: {
+      inEdit(){
+        if(!this.edit){
+          this.edit = true;
+          console.log(this.edit)
+        }
+      },
+      inFulfil(){
+        if(this.edit){
+          this.edit = false;
+        }
+      },
+      dialogLeave(){
+        this.leave = true;
+      },
+      dialogJoin(){
+        this.join = true;
+      },
+      isLeaveSubmit(){
+        this.leave = false;
+      },
+      isLeaveCancel(){
+        this.leave= false;
+      },
+      isJoinSubmit(){
+        this.join = false;
+      },
+      isJoinCancel(){
+        this.join = false;
+      }
+    },
+    mounted(){
+      this.rows = [{name:"瑜伽活动",action:"江西省南昌市新建区赣江南大道1366号省公安厅201室"}]
+      }
+    
   };
 </script>
 
