08156d7efd9d4ed798c07917d3d4db4ebb2fcc41
金振南 <516636965@qq.com>
2019-01-22 10:37:24 +0800

    解决BUG 1.0

--- src/apis/police/conf.js
+++ src/apis/police/conf.js
@@ -3,7 +3,11 @@ export default [
 		// 查询活动设置
 		verb: "get",
 		outKey: "getInfoActivity",
-		action: "mobile/v1/infoActivity/selectById",
+		action: "/mobile/v1/user/getAllUser",
+		reqParams: [
+			{ name: "page", default: 1 },
+			{ name: "pageSize", default: 10 }
+		]
     },
     {
 		// 获得所有参与人员
