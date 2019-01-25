4fd561775a11f778c202cdbb23466940d80885f3
金振南 <516636965@qq.com>
2019-01-25 17:23:45 +0800

    添加自动排课

--- src/views/police/personnel.vue
+++ src/views/police/personnel.vue
@@ -1,120 +1,57 @@
-<style lang="scss">
-  @import "../../assets/modules/police/view-person.scss";
-</style>
 <template>
-    <!--<div class="person">-->
-      <!--<Header left="取消"-->
-              <!--right="完成"-->
-              <!--title="选择参与人员"></Header>-->
-      <!--<div class="person-header"><div></div><div></div><div></div></div>-->
-      <!--<div class="list">-->
-        <!--<div class="list-header">A</div>-->
-        <!--<div class="list-item" v-for="i in person">-->
-          <!--<div class="choose">-->
-            <!--<input type="checkbox" :id="i.id" >-->
-            <!--<label :for="i.id" :class="[isWebp()?'labelWebp':'']">-->
-            <!--</label>-->
-          <!--</div>-->
-          <!--<div class="list-pic"><div></div></div>-->
-          <!--<div class="person-name">{{i.name}}</div>-->
-        <!--</div>-->
-      <!--</div>-->
-      <!--<div class="aside">-->
-        <!--<ul>-->
-          <!--<li v-for="i in cityData">{{i}}</li>-->
-        <!--</ul>-->
-      <!--</div>-->
-    <!--</div>-->
-	<mt-index-list>
-    <Header left="取消"
-            :right="'完成'+'('+(num)+')'"
-            title="选择参与人员"></Header>
-    <div class="person-header"><div></div><div></div><div></div></div>
-		<mt-index-section  v-for="(item,index) in alphabet" :key="item.initial">
-			<mt-cell v-for="(cell,index) in item.cells" :key="index">
-      <div class="choose">
-        <input type="checkbox" 
-               :id="cell" 
-               v-model="checkRows"
-               ref="check"
-               :value="cell">
-        <label :for="cell" :class="[isWebp()?'labelWebp':'']">
-        </label>
-      </div>
-      <div class="list-pic"><div></div></div>
-      <div class="person-name">{{cell}}</div>
-      </mt-cell>
-		</mt-index-section>
-	</mt-index-list>
+    <div @click="__fetch()" 
+         class="class"><button>点击自动排课</button>
+    <toast :text="toast_text"
+           :showToast="showToast"></toast>
+    </div>
 </template>
-
 <script>
-import Vue from 'vue'
-import { utils } from "~rx";
-import { IndexList, IndexSection } from 'mint-ui';
-import { Cell } from 'mint-ui';
-Vue.component(Cell.name, Cell);
-Vue.component(IndexList.name, IndexList);
-Vue.component(IndexSection.name, IndexSection);
-	const NAMES = ['Aaron', 'Alden', 'Austin', 'Baldwin', 'Braden', 'Carl', 'Chandler', 'Clyde', 'David', 'Edgar', 'Elton', 'Floyd', 'Freeman', 'Gavin', 'Hector', 'Henry', 'Ian', 'Jason', 'Joshua', 'Kane', 'Lambert', 'Matthew', 'Morgan', 'Neville', 'Oliver', 'Oscar', 'Perry', 'Quinn', 'Ramsey', 'Scott', 'Seth', 'Spencer', 'Timothy', 'Todd', 'Trevor', 'Udolf', 'Victor', 'Vincent', 'Walton', 'Willis', 'Xavier', 'Yvonne', 'Zack', 'Zane'];
 export default {
-  components: {
-			Header: () =>
-				import(/* webpackChunkName:"wc-header" */ "@/views/police/__wc__/header/header.vue").then(
-					utils.fixAsyncCmpLifeCycle
-				),
-		},
-  data(){
-    return {
-      checkRows:[],
-      person:[],
-      alphabet: [],
-      num:0
-    }
-  },
-  watch: {
-    checkRows(){
-      this.num = this.checkRows.length;
-      if(this.num==25){
-        this.$refs.check.forEach(check => {
-          if(!check.checked){
-            check.disabled=true
-          }
-        })
-      }else {
-        this.$refs.check.forEach(check => {
-          check.disabled=false
-        })
-      }
-    }
-  },
-	created(){
-		'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').forEach(initial => {
-			let cells = NAMES.filter(name => name[0] === initial);
-			this.alphabet.push({
-				initial,
-				cells
-			});
-		});
-	},
-  methods: {
-    __fetch(){
-      this.person=[
-        {name:"钢铁侠",id:"1"},
-        {name:"蜘蛛侠",id:"2"},
-        {name:"铁蛋侠",id:"3"},
-        {name:"钢铁侠",id:"4"},
-        {name:"蜘蛛侠",id:"5"},
-        {name:"铁蛋侠",id:"6"},
-        {name:"钢铁侠",id:"7"},
-        {name:"蜘蛛侠",id:"8"},
-        {name:"铁蛋侠",id:"9"}];
+    components: {
+        toast: () =>
+  			import(/* webpackChunkName:"police-phone-toast" */ "~v/police/__wc__/phone-toast.vue")
+  	},
+    data(){
+        return {
+            test:[],
+            toast_text: "",
+            showToast: false
+        }
     },
-    getValue(list){
+    methods: {
+      async  __fetch(){
+        const [err, resp] = await this.$sync(this.$http.police.init());
+          if(!err){
+            this.test = resp.result
+            console.log("成功")
+            this.toast_text = "排课成功!~";
+					this.toast();
+        }else{
+            console.log(err)
+            this.toast_text = "请重试";
+					this.toast();
+        }
     },
-  },
-  mounted(){
-    this.__fetch();
-  }
+    toast() {
+  			this.showToast = true;
+  			setTimeout(() => {
+  				this.showToast = false;
+  			}, 1500);
+  		},
+    },
+    created() {
+    },
 }
 </script>
+<style lang="scss">
+    .class{
+        button {
+            position: relative;
+            top:50%;
+            left:50%;
+            margin-left:-100px;
+            width:200px;
+            height:100px;
+        }
+    }
+</style>
